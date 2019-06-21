6cc99cbf01ca0a77d030a2657b10dc8c1cd03e3a
ojtanner <oliver-tanner@outlook.com>
2019-06-21 19:52:29 +0200

    Change slot to p due to inability to style

--- src/modules/Reusable/Button.svelte
+++ src/modules/Reusable/Button.svelte
@@ -2,21 +2,23 @@
 </script>
 
 <style>
+
     #button {
         width: 100%;
         height: 100%;
 
-        background-color: #d8dee9;
+        background-color: #81a1c1;
         color: #3b4252;
         font-size: 1.5em;
         border-radius: 5%;
+    }
 
-        padding: 0;
-        margin: 0;
+    #button > p {
+        margin: 0;   
     }
 
 </style>
 
 <button type="button" id="button">
-    <slot></slot>
+    <p>Reset game</p>
 </button>
