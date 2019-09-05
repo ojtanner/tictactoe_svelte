724a7a7c6977ec395dba4cec1b2d83772ac3b5c3
ojtanner <oliver-tanner@outlook.com>
2019-09-05 21:40:15 +0200

    Remove Console.Log

--- src/js/gameLogicStore.js
+++ src/js/gameLogicStore.js
@@ -10,9 +10,7 @@ const writableStore = writable(gameLogic.getFieldState(), () => {
 const PlayingFieldStore = {
     subscribe: writableStore.subscribe,
     executeTurn: (fieldIndex) => {
-        console.log(`Executing turn with field ${fieldIndex}`);
         gameLogic.executeTurn(fieldIndex);
-        console.log('New fieldstate is', gameLogic.getFieldState())
         writableStore.set(gameLogic.getFieldState());
     },
     resetGame: () => {
